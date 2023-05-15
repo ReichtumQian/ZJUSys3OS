d7448746f6c1ff6ab6808474871aff3168ffc4a0
reichtum <qyx18267223529@outlook.com>
2023-05-15 15:09:10 +0000

    Fix bug in entry.S

--- arch/riscv/kernel/trap.c
+++ arch/riscv/kernel/trap.c
@@ -1,17 +1,19 @@
-// trap.c 
-#include"clock.h"
-#include"printk.h"
-#include "types.h"
-#include"proc.h"
+// trap.c
+#include "clock.h"
+#include "printk.h"
+#include "proc.h"
 #include "syscall.h"
+#include "types.h"
 
-struct pt_regs{
+
+struct pt_regs {
   uint64 x[31];
   uint64 sepc;
   uint64 sstatus;
 };
 
-void trap_handler(unsigned long scause, unsigned long sepc, struct pt_regs *regs) {
+void trap_handler(unsigned long scause, unsigned long sepc,
+                  struct pt_regs *regs) {
   // judge trap type via scause
   unsigned long mask = 1;
   mask = mask << 63;
@@ -24,16 +26,16 @@ void trap_handler(unsigned long scause, unsigned long sepc, struct pt_regs *regs
       clock_set_next_event();
       do_timer();
     }
-  }else{
-    if(scause == 8){
-      if(regs->x[17] == SYS_WRITE){
-        sys_write(regs->x[10], (char*)regs->x[11], regs->x[12]);
-      }else if(regs->x[17] == SYS_GETPID){
+  } else {
+    if (scause == 8) {
+      if (regs->x[17] == SYS_WRITE) {
+        sys_write(regs->x[10], (char *)regs->x[11], regs->x[12]);
+      } else if (regs->x[17] == SYS_GETPID) {
         regs->x[10] = sys_getpid();
       }
-      regs -> sepc += 4;
+      regs->sepc += 4;
     }
   }
 
-  return ;
+  return;
 }
